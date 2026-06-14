7ca32ef72a8c7e612dca8a72e9328637bce446e6
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-06-14 09:43:11 +0530

    functions

---
+++ dist/functionTs.js
@@ -0,0 +1,11 @@
+"use strict";
+// function hello(name:string) {
+//     console.log(`Hello ${name}`)
+Object.defineProperty(exports, "__esModule", { value: true });
+// }
+//hello('Alice')
+function hello(name = "Bob") {
+    console.log(`Hello ${name}`);
+}
+hello();
+//# sourceMappingURL=functionTs.js.map
--- src/functionTs.ts
+++ src/functionTs.ts
@@ -1,5 +1,14 @@
-function hello(name:string) {
+// function hello(name:string) {
+//     console.log(`Hello ${name}`)
+    
+// }
+//hello('Alice')
+
+// function hello(name:string="Bob") {
+//     console.log(`Hello ${name}`)
+// }
+function hello(name?:string) {
     console.log(`Hello ${name}`)
     
 }
-hello('Alice')
+hello()
